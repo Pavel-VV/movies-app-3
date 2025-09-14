a5add53adc7a919ab1297cb5ec17b696a30fc695
Pavel-VV <vahonin7@mail.ru>
2025-09-14 23:50:25 +0300

    ending store

--- src/plagins/axios/interceptors.js
+++ src/plagins/axios/interceptors.js
@@ -1,5 +1,4 @@
 function setParams(config) {
-  console.log(config);
   const params = config.params || {};
   config.params = Object.assign(params, {
     apikey: process.env.VUE_APP_API_KEY,
@@ -7,7 +6,11 @@ function setParams(config) {
   });
   return config;
 }
+function responseData(response) {
+  return response.data;
+}
 
 export default function (axios) {
   axios.interceptors.request.use(setParams);
+  axios.interceptors.response.use(responseData);
 }
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,19 +1,60 @@
 import axios from "@/plagins/axios";
+import mutations from "@/store/mutations";
+import IDs from "@/store/mock/imdb_top250";
+
+function serializeMovies(movies) {
+  const newList = movies.reduce((acc, movie) => {
+    acc[movie.imdbID] = movie;
+    return acc;
+  }, {});
+  return newList;
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+    movies: {},
+  },
+  getters: {
+    slicedIDs:
+      ({ top250IDs }) =>
+      (from, to) =>
+        top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
+  mutations: {
+    [MOVIES](state, value) {
+      state.movies = value;
+    },
+  },
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      const response = await axios.get("/", {
-        params: {
-          i: "tt0111161",
-        },
-      });
-      console.log(response);
+    async fetchMovies({ getters, commit }) {
+      try {
+        const { slicedIDs, moviesPerPage, currentPage } = getters;
+        const from = currentPage * moviesPerPage - moviesPerPage;
+        const to = currentPage * moviesPerPage;
+        const moviesToFetch = slicedIDs(from, to);
+        console.log(moviesToFetch);
+        const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+        console.log(requests);
+        // const response = await axios.get("/", {
+        //   params: {
+        //     i: "tt0111161",
+        //   },
+        // });
+        const response = await Promise.all(requests);
+        const movies = serializeMovies(response);
+        console.log(movies);
+        commit(MOVIES, movies);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
