59a7e514dde00acd179830942669a8bbb367d887
Pavel-VV <vahonin7@mail.ru>
2025-09-17 23:31:44 +0300

    add component MoviesItem

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="movie-item" :style="background">{{ movie.Title }}</div>
+</template>
+
+<script>
+export default {
+  name: "MovieItem",
+  props: {
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    background() {
+      return {
+        "background-image": `url( ${this.movie.Poster} )`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.movie-item {
+  border-radius: 5px;
+  margin-bottom: 30px;
+  height: 400px;
+  cursor: pointer;
+  /* background-color: black; */
+}
+</style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -3,16 +3,22 @@
     <h3 class="list-title">IMDB top 250</h3>
     <BRow>
       <template v-if="isExist">
-        <BCol cols="3" v-for="(movie, key) in list" :key="key">{{ key }}</BCol>
+        <BCol cols="3" v-for="(movie, key) in list" :key="key">
+          <MovieItem :movie="movie" />
+        </BCol>
       </template>
-      <div v-else>Empty List</div>
+      <template v-else>
+        <div>Empty List</div>
+      </template>
     </BRow>
   </BContainer>
 </template>
 
 <script>
+import MovieItem from "@/components/MovieItem";
 export default {
   name: "MoviesList",
+  components: { MovieItem },
   props: {
     list: {
       type: Object,
