fb74e01d888e4678c0c55cbd0c9183f2fc5a232f
Pavel-VV <vahonin7@mail.ru>
2025-09-19 23:18:28 +0300

    add movie info and button control item

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="movie-item">
     <div class="movie-item-poster" :style="posterBackground"></div>
+    <div class="movie-info-wrap">
+      <div class="movie-info">
+        <h3 class="movie-title">{{ movie.Title }}</h3>
+        <span class="movie-year">{{ movie.Year }}</span>
+      </div>
+      <div class="control-panel">
+        <BButton class="control-btn mr-2" variant="outline-light">Edit</BButton>
+        <BButton class="control-btn ml-2" variant="outline-light"
+          >Delete</BButton
+        >
+      </div>
+    </div>
   </div>
 </template>
 
@@ -48,5 +60,40 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  z-index: -1;
+}
+
+.movie-info-wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px 10px;
+  opacity: 0;
+  transition: all 0.2s ease;
+  height: 100%;
+}
+
+.movie-item:hover .movie-info-wrap {
+  opacity: 1;
+  background-color: rgba(0, 0, 0, 0.7);
+}
+
+.movie-title {
+  font-size: 20px;
+  color: #fff;
+}
+
+.movie-year {
+  font-size: 14px;
+  color: #fff;
+}
+
+.control-panel {
+  display: flex;
+  margin-top: 0;
+}
+
+.control-btn {
+  width: 100%;
 }
 </style>
