4859c40692a4ea78c5f37408c7da6f727262c7bf
Pavel-VV <vahonin7@mail.ru>
2025-11-20 22:38:27 +0300

    add style on movie poster info

--- src/components/MovieInfoModal.vue
+++ src/components/MovieInfoModal.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="info-modal">
     <header class="header">
-      <h6>Movie info</h6>
+      <h6 class="movie-header-title">Movie info</h6>
       <p>{{ movie.Title }}</p>
       <BIcon class="close-info" icon="x" @click="eventCloseInfo" />
     </header>
+    <div class="movie-info-wrap">
+      <BRow>
+        <BCol sm="4">
+          <div class="movie-poster-wrap">
+            <div class="movie-poster" :style="backgroundStyle"></div>
+          </div>
+        </BCol>
+        <BCol class="movie-info" sm="8"></BCol>
+      </BRow>
+    </div>
   </div>
 </template>
 
@@ -17,9 +27,24 @@ export default {
       default: () => ({}),
     },
   },
+  data: () => ({
+    defaultPoster: "linear-gradient(45deg, #270133, #940294)",
+  }),
   methods: {
     eventCloseInfo() {
       this.$emit("onEventCloseInfo");
+    },
+  },
+  computed: {
+    backgroundPoster() {
+      return this.movie.Poster
+        ? `url(${this.movie.Poster})`
+        : this.defaultPoster;
+    },
+    backgroundStyle() {
+      return {
+        "background-image": `${this.backgroundPoster}`,
+      };
     },
   },
 };
@@ -30,12 +55,39 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 1rem;
   background-image: linear-gradient(45deg, #270133, #940294);
   color: #fff;
+}
+
+.movie-header-title {
+  margin-bottom: 0;
 }
 
 .close-info {
   font-size: 1.5rem;
   cursor: pointer;
+}
+
+.movie-info-wrap {
+  background-color: #fff;
+  padding: 1rem;
+}
+
+.movie-poster-wrap {
+  position: relative;
+  padding-bottom: 150%;
+  overflow: hidden;
+  border-radius: 5px;
+}
+
+.movie-poster {
+  position: absolute;
+  background-size: cover;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center center;
 }
 </style>
